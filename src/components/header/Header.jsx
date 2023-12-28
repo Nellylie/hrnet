4172a36ceec46f2c0346e2e5adf6f60b036f9e71
Nellylie <marie.beka01@gmail.com>
2023-12-28 13:26:12 +0100

    feat(): style update + fix navigation

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,30 @@
-import { useState } from "react";
-import { useNavigate } from "react-router";
+import { useState, useEffect } from "react";
+import { useNavigate, useLocation } from "react-router";
+import logo from "../../assets/logo.png"
+import { Link } from "react-router-dom";
+
 function Header() {
     const homeUrl = '/';
     const employeeList = '/employees';
-    const [switchUrl, setSwitchUrl] = useState(homeUrl);
     const navigate = useNavigate();
+    const [switchUrl, setSwitchUrl] = useState(homeUrl);
+    const location = useLocation();
+
+    useEffect(() => {
+        setSwitchUrl(location.pathname === homeUrl ? employeeList : homeUrl);
+    }, [location]); 
+
     const toggleUrl = () => {
-        const newUrl = switchUrl === homeUrl ? employeeList : homeUrl;
-        setSwitchUrl(newUrl);
-        navigate(newUrl);    }
+        navigate(switchUrl); 
+    };
+
 
     return (
         <div className="header-content">
+        <div className="header-logo"><Link to="/"><img src={logo} alt="hrnet logo"></img></Link></div>
             <h1>HRnet</h1>
             <button onClick={toggleUrl}>
-                {switchUrl === homeUrl ? 'Go to Employees List' : 'Create New Employee'}
+                {switchUrl === homeUrl ?  'Create New Employee' : 'Go to Employees List'}
             </button>
         </div>
     )
